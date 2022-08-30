b40c5ea1b35214873f07b34a2667f0fb5822c984
AndyI0I <92602614+AndyI0I@users.noreply.github.com>
2022-08-30 10:35:23 +0200

    adsf

---
+++ Linq/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Programm
+{
+    public class MainApp
+    {
+
+
+        public void run()
+        {
+            
+        }
+
+
+
+        public static void Main(string[] vs)
+        {
+            MainApp mainApp = new();
+
+            mainApp.run();
+        }
+    }
+}
---
+++ Linq/dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].Mitarbeiter
+(
+	PersonalNummer INT NOT NULL PRIMARY KEY,
+	Vorname VARCHAR(255),
+	Nachname VARCHAR(255),
+	Eintrittsdatum DATETIME
+)
